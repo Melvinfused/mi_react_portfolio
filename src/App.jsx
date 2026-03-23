6d1fa32a5130478fac3621f9ee656ba96897e54b
Melvin Francy <thomasfrancymoothedath0@gmail.com>
2026-03-23 17:57:17 +0530

    Chromatic Aberration disable on firefox.

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,15 @@ import "./editmode.css";
 const App = () => {
   return (
     <>
-      <svg style={{ position: "absolute", visibility: "hidden", pointerEvents: "none" }} aria-hidden="true">
-        <filter id="ca-glass" x="-10%" y="-10%" width="120%" height="120%" colorInterpolationFilters="sRGB">
-          <feGaussianBlur in="SourceGraphic" stdDeviation="4" edgeMode="duplicate" result="blurred" />
-          <feColorMatrix in="blurred" type="matrix" result="red" values="1 0 0 0 0  0 0 0 0 0  0 0 0 0 0  0 0 0 1 0" />
-          <feConvolveMatrix in="red" order="15 1" kernelMatrix="0 0 0 0 0 0 0 0 0 0 0 0 0 0 1" edgeMode="duplicate" result="red_shifted" />
-          <feColorMatrix in="blurred" type="matrix" result="green" values="0 0 0 0 0  0 1 0 0 0  0 0 0 0 0  0 0 0 1 0" />
-          <feColorMatrix in="blurred" type="matrix" result="blue" values="0 0 0 0 0  0 0 0 0 0  0 0 1 0 0  0 0 0 1 0" />
-          <feConvolveMatrix in="blue" order="15 1" kernelMatrix="1 0 0 0 0 0 0 0 0 0 0 0 0 0 0" edgeMode="duplicate" result="blue_shifted" />
+      <svg style={{ position: "absolute", width: 0, height: 0 }} aria-hidden="true">
+        <filter id="ca-glass">
+          <feColorMatrix in="SourceGraphic" type="matrix" result="red" values="1 0 0 0 0  0 0 0 0 0  0 0 0 0 0  0 0 0 1 0" />
+          <feOffset in="red" dx="4" dy="0" result="red_shifted_raw" />
+          <feBlend mode="lighten" in="red_shifted_raw" in2="red" result="red_shifted" />
+          <feColorMatrix in="SourceGraphic" type="matrix" result="green" values="0 0 0 0 0  0 1 0 0 0  0 0 0 0 0  0 0 0 1 0" />
+          <feColorMatrix in="SourceGraphic" type="matrix" result="blue" values="0 0 0 0 0  0 0 0 0 0  0 0 1 0 0  0 0 0 1 0" />
+          <feOffset in="blue" dx="-4" dy="0" result="blue_shifted_raw" />
+          <feBlend mode="lighten" in="blue_shifted_raw" in2="blue" result="blue_shifted" />
           <feBlend mode="screen" in="red_shifted" in2="green" result="rg" />
           <feBlend mode="screen" in="rg" in2="blue_shifted" result="rgb" />
         </filter>
